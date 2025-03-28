a8c0879c79128da1562778b498cef150ccc16217
Aobies59 <alvaroobiesgarcia@proton.me>
2025-03-28 09:33:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,169 @@
+const windowTitles = Array.from(document.getElementsByClassName("title"));
+const footerButtons = Array.from(
+  document.getElementsByClassName("footer_button"),
+);
+const closeButtons = Array.from(document.getElementsByClassName("title_close"));
+
+const desktop = document.getElementById("desktop");
+const desktopSelector = document.createElement("div");
+desktopSelector.id = "selector";
+desktop.appendChild(desktopSelector);
+let initialSelectorPosition = { x: 0, y: 0 };
+desktop.addEventListener("pointerdown", (event) => {
+  initialSelectorPosition.x = event.clientX;
+  initialSelectorPosition.y = event.clientY;
+  desktopSelector.style.left = event.clientX + "px";
+  desktopSelector.style.top = event.clientY + "px";
+  desktopSelector.style.width = 0;
+  desktopSelector.style.height = 0;
+  desktopSelector.style.display = "flex";
+});
+desktop.addEventListener("pointerleave", () => {
+  desktopSelector.style.display = "none";
+});
+
+
+const footer = document.querySelector("footer");
+closeButtons.forEach((currButton) => {
+  currButton.addEventListener("click", () => {
+    const window = currButton.parentNode.parentNode;
+    const footerButtons = Array.from(footer.querySelectorAll("div"))
+    for (let i=0; i<footerButtons.length; i++) {
+      const currButton = footerButtons[i];
+      if (currButton.dataset.window == window.id) {
+        currButton.remove();
+        break;
+      }
+    }
+    window.remove();
+
+  });
+});
+
+let selectedWindow = null;
+let previouslySelectedWindow = null;
+windowTitles.forEach((currTitle) => {
+  const currWindow = currTitle.parentNode;
+  const footerButton = document.createElement("div");
+  footerButton.classList.add("footer_button", "bordered");
+  footerButton.dataset.window = currWindow.id;
+  footerButton.innerHTML = currWindow.id;
+  footer.insertBefore(footerButton, document.getElementById("filler"));
+
+  currTitle.addEventListener("pointerdown", () => {
+    selectedWindow = currWindow;
+    selectedWindow.style.zIndex = 3;
+  });
+});
+
+let clickedButton = null;
+const clickedButtonClassName = "clicked";
+footerButtons.forEach((currButton) => {
+  currButton.addEventListener("pointerdown", (event) => {
+    currButton.classList.remove(clickedButtonClassName);
+    if (currButton != clickedButton) {
+      clickedButton = currButton;
+      clickedButton.classList.add(clickedButtonClassName);
+    } else {
+      clickedButton = null;
+    }
+
+    event.stopPropagation();
+  });
+});
+
+document.addEventListener("pointerup", () => {
+  if (selectedWindow != null) {
+    selectedWindow.style.zIndex = 2;
+    if (previouslySelectedWindow != null) {
+      previouslySelectedWindow.style.zIndex = 1;
+    }
+    previouslySelectedWindow = selectedWindow;
+  }
+  selectedWindow = null;
+  desktopSelector.style.display = "none";
+});
+
+document.addEventListener("pointerdown", () => {
+  if (clickedButton != null) {
+    clickedButton.classList.remove(clickedButtonClassName);
+    clickedButton = null;
+  }
+});
+
+let previousPointerPosition = null;
+document.addEventListener("pointermove", (event) => {
+  if (previousPointerPosition == null) {
+    previousPointerPosition = { x: event.clientX, y: event.clientY };
+    return;
+  }
+
+  const dX = event.clientX - previousPointerPosition["x"];
+  const dY = event.clientY - previousPointerPosition["y"];
+
+  if (selectedWindow != null) {
+    const windowRect = selectedWindow.getBoundingClientRect();
+    const bodyRect = document.body.getBoundingClientRect();
+
+    const windowTop = Math.min(
+      Math.max(
+        windowRect.top + dY,
+        0
+      ),
+      bodyRect.height - 60
+    )
+    const windowLeft = Math.max(windowRect.left + dX,0);
+
+    selectedWindow.style.left = windowLeft + "px";
+    selectedWindow.style.top = windowTop + "px";
+  } else {
+    const xDiff = initialSelectorPosition.x - event.clientX;
+    if (xDiff > 0) {
+      desktopSelector.style.left = event.clientX + "px";
+      desktopSelector.style.width = xDiff + "px";
+    } else {
+      desktopSelector.style.left = initialSelectorPosition.x;
+      desktopSelector.style.width = -xDiff + "px";
+    }
+
+    const yDiff = initialSelectorPosition.y - event.clientY;
+    if (yDiff > 0) {
+      desktopSelector.style.top = event.clientY + "px";
+      desktopSelector.style.height = yDiff + "px";
+    } else {
+      desktopSelector.style.top = initialSelectorPosition.y;
+      desktopSelector.style.height = -yDiff + "px";
+    }
+  }
+
+  previousPointerPosition = { x: event.clientX, y: event.clientY };
+});
+
+let timerSeconds = 0;
+let timerMinutes = 0;
+const maxTimerValue = 60;
+const secondsContainer = document.getElementById("timer-seconds");
+const minutesContainer = document.getElementById("timer-minutes");
+setInterval(
+  () => {
+    timerSeconds += 1;
+    if (timerSeconds >= maxTimerValue) {
+      timerMinutes = Math.min(timerMinutes + 1, maxTimerValue);
+      timerSeconds = 0;
+    }
+
+    let secondsString = String(timerSeconds);
+    if (timerSeconds < 10) {
+      secondsString = "0" + secondsString;
+    }
+
+    let minutesString = String(timerMinutes);
+    if (timerMinutes < 10) {
+      minutesString = "0" + minutesString;
+    }
+
+    secondsContainer.innerHTML = secondsString;
+    minutesContainer.innerHTML = minutesString;
+  },
+  1000
+);
