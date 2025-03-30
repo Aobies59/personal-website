86f44c4f70d764947909f12a886e5504c6f71419
Aobies59 <alvaroobiesgarcia@proton.me>
2025-03-30 10:40:57 +0200

    use a function to create windows

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const windowTitles = Array.from(document.getElementsByClassName("title"));
 const footer = document.querySelector("footer");
 let footerButtons = Array.from(footer.querySelectorAll("div"));
-const closeButtons = Array.from(document.getElementsByClassName("title_close"));
 const desktop = document.getElementById("desktop");
 
-// click start bar buttons
 let clickedButton = null;
 const activeButtonClassName = "active";
 function selectStartBarButton(button) {
@@ -32,7 +29,6 @@ function updateFooterButtonsList() {
 }
 updateFooterButtonsList();
 
-// desktop selector
 const desktopSelector = document.createElement("div");
 desktopSelector.id = "selector";
 desktop.appendChild(desktopSelector);
@@ -51,7 +47,6 @@ desktop.addEventListener("pointerdown", (event) => {
   deHighlightWindow();
 });
 
-// window close buttons
 function closeWindow(window) {
   for (let i = 0; i < footerButtons.length; i++) {
     const currButton = footerButtons[i];
@@ -63,13 +58,7 @@ function closeWindow(window) {
   }
   window.remove();
 }
-closeButtons.forEach((currButton) => {
-  currButton.addEventListener("click", () => {
-    closeWindow(currButton.parentNode.parentNode);
-  });
-});
 
-// highlight windows
 let highlightedWindow = null;
 let previouslyHighlightedWindow = null;
 let highlightedWindowFooterButton = null;
@@ -109,9 +98,6 @@ function deHighlightWindow() {
   }
 }
 
-// select windows when clicking on title bar
-// create start bar buttons for each window
-let selectedWindow = null;
 let previouslySelectedWindow = null;
 const startBarFiller = document.getElementById("filler");
 function createStartBarButton(windowId) {
@@ -122,22 +108,53 @@ function createStartBarButton(windowId) {
   footer.insertBefore(footerButton, startBarFiller);
   updateFooterButtonsList();
 }
+
+let selectedWindow = null;
 function selectWindow(window) {
   selectedWindow = window;
   highlightWindow(window);
 }
-windowTitles.forEach((currTitle) => {
-  const currWindow = currTitle.parentNode;
-  createStartBarButton(currWindow.id);
 
-  currTitle.addEventListener("pointerdown", () => {
-    selectWindow(currWindow);
-  });
+function createWindow(windowId, left, top, title, content, windowType=null, scrollable=false) {
+  const windowDiv = document.createElement("div");
+  windowDiv.classList.add("window", "bordered");
+  if (windowType != null) {
+    windowDiv.classList.add(windowType)
+  }
+  windowDiv.id = windowId;
+  windowDiv.style.left = left;
+  windowDiv.style.top = top;
 
-  currWindow.addEventListener("pointerdown", () => {
-    highlightWindow(currWindow);
-  });
-});
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("title");
+  titleDiv.innerHTML = title;
+  titleDiv.addEventListener("pointerdown", () => {
+    selectWindow(windowDiv);
+  })
+  const closeButtonDiv = document.createElement("div");
+  closeButtonDiv.classList.add("title_close", "bordered");
+  closeButtonDiv.innerHTML = "x";
+  closeButtonDiv.addEventListener("click", () => {
+    closeWindow(windowDiv);
+  })
+  titleDiv.appendChild(closeButtonDiv);
+  windowDiv.appendChild(titleDiv);
+
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("content");
+  if (scrollable) {
+    contentDiv.classList.add("scrollable");
+  }
+  contentDiv.innerHTML = content;
+  windowDiv.appendChild(contentDiv);
+
+  windowDiv.addEventListener("pointerdown", () => {
+    highlightWindow(windowDiv);
+  })
+
+  document.body.appendChild(windowDiv);
+  createStartBarButton(windowId);
+}
 
 // deselect window when lifting pointer
 document.addEventListener("pointerup", () => {
@@ -192,33 +209,6 @@ document.addEventListener("pointermove", (event) => {
 
   previousPointerPosition = { x: event.clientX, y: event.clientY };
 });
-
-// update timer
-let timerSeconds = 0;
-let timerMinutes = 0;
-const maxTimerValue = 60;
-const secondsContainer = document.getElementById("timer-seconds");
-const minutesContainer = document.getElementById("timer-minutes");
-setInterval(() => {
-  timerSeconds += 1;
-  if (timerSeconds >= maxTimerValue) {
-    timerMinutes = Math.min(timerMinutes + 1, maxTimerValue);
-    timerSeconds = 0;
-  }
-
-  let secondsString = String(timerSeconds);
-  if (timerSeconds < 10) {
-    secondsString = "0" + secondsString;
-  }
-
-  let minutesString = String(timerMinutes);
-  if (timerMinutes < 10) {
-    minutesString = "0" + minutesString;
-  }
-
-  secondsContainer.innerHTML = secondsString;
-  minutesContainer.innerHTML = minutesString;
-}, 1000);
 
 // update clock
 const clockHoursContainer = document.getElementById("clock_hours");
@@ -319,5 +309,52 @@ document.addEventListener("pointerdown", () => {
   startMenu.classList.remove("active");
   startButton.classList.remove(activeButtonClassName);
 });
+
+createWindow("welcome", "10%", "10%", "Welcome!",
+  "Welcome to my personal website!<br>\
+  Here you will find the random stuff I make when I'm bored.<br>\
+  Hope you like it!",
+  "window-text");
+
+createWindow("todo", "40%", "30%", "To Do:", 
+  "&check; Make the clock in the bottom right actually work<br />\
+  &check; Create scrollable windows (with custom scrollbar)<br />\
+  &check; Make it so you can focus windows using the start bar<br />\
+  &check; Add some functionality to the start button<br />\
+  - Add some functionality to the start menu<br />\
+  - Add minimizing/maximing windows<br />\
+  - Add icons to the desktop (with the ability to select, click and open them)<br />\
+  - Add a portfolio page<br />\
+  - Add a resume page<br />\
+  - Minesweeper<br />",
+  "window-big", true);
+
+createWindow("timer", "20%", "50%", "Timer",
+  "<span id='timer-minutes'>00</span>:<span id='timer-seconds'>00</span>", "window-small")
+let timerSeconds = 0;
+let timerMinutes = 0;
+const maxTimerValue = 60;
+const secondsContainer = document.getElementById("timer-seconds");
+const minutesContainer = document.getElementById("timer-minutes");
+setInterval(() => {
+  timerSeconds += 1;
+  if (timerSeconds >= maxTimerValue) {
+    timerMinutes = Math.min(timerMinutes + 1, maxTimerValue);
+    timerSeconds = 0;
+  }
+
+  let secondsString = String(timerSeconds);
+  if (timerSeconds < 10) {
+    secondsString = "0" + secondsString;
+  }
+
+  let minutesString = String(timerMinutes);
+  if (timerMinutes < 10) {
+    minutesString = "0" + minutesString;
+  }
+
+  secondsContainer.innerHTML = secondsString;
+  minutesContainer.innerHTML = minutesString;
+}, 1000);
 
 console.log("If you are reading this follow me :)");
